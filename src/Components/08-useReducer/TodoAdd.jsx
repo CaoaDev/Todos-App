098479afba39cb333d6ad25d4d417ec010fa85a0
GabrielRIP <jgrcreations@gmail.com>
2022-08-02 23:01:29 -0500

    se da mucho estilo

--- src/Components/08-useReducer/TodoAdd.jsx
+++ src/Components/08-useReducer/TodoAdd.jsx
@@ -32,17 +32,24 @@ export const TodoAdd = ({ handleAddTodo, dispatch }) => {
             type="text"
             id="titleForm"
             name="description"
-            placeholder=' '
+            placeholder=" "
             value={description}
             onChange={handleInputChange}
           />
           <label htmlFor="titleForm">Add Title</label>
         </div>
         <div className={styles.boxTextarea}>
-          <textarea placeholder="hola verga"></textarea>
-          <span>hola</span>
+          <textarea placeholder=" " />
+          <span>Add Text</span>
         </div>
-        <button type="submit">Add</button>
+        <button type="submit" className={styles.buttonSubmit}>
+          <span>Add</span>
+          <span>(</span>
+          <svg viewBox="0 0 448 512" className={styles.iconAdd}>
+            <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
+          </svg>
+          <span>)</span>
+        </button>
       </form>
     </div>
   )
